80af941d71dcbb0508b910ff58f8325ce12d0413
lilywebe <55253338+lilywebe@users.noreply.github.com>
2021-09-07 13:47:35 -0400

    Update app.js

--- Labs/lab2/app.js
+++ Labs/lab2/app.js
@@ -9,26 +9,31 @@ class Instrument{
         console.log(this.family + " "+this.verb+" at "+this.loudness);
     }
 }
+
 //define woodwind class
 class Woodwind extends Instrument{
     constructor(loudness){
         super("Woodwind", loudness, "blows");
     }
 }
+
 //define strings class
 class Strings extends Instrument{
     constructor(loudness){
         super("Strings", loudness, "pluck");
     }
 }
+
 //define percussion class
 class Percussion extends Instrument{
     constructor(loudness){
         super("Percussion", loudness, "beats");
     }
 }
+
 //place instruments in array
 instruments = [new Percussion("high volume"), new Woodwind("a whisper level"), new Strings("medium volume")];
+
 //play all instruments
 instruments.forEach(instrument => {
     instrument.play(); 
