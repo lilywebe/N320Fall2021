12abf9ecfd66635bd00d95ada5fe77abcdd2cc8c
lilywebe <55253338+lilywebe@users.noreply.github.com>
2021-10-03 20:03:07 -0400

    added finishing locations

--- Trouble/app.js
+++ Trouble/app.js
@@ -6,9 +6,19 @@ constructor(){
     this.colors=["red", "yellow", "green", "blue"];
 }
 updateforroll(){
+        //maybe shouldnt go here idk
+        this.pieces.forEach(piece => {
+            if(piece.color==this.playerturn){
+                //find potential moves within the current players pegs
+                //check if piece overlaps with a piece of a DIFFERENT COLOR to relocate other piece
+                //check if piece overlaps with a piece of the SAME color to eliminate potential movement
+            }
+            
+        });
 
 }
 makemove(){
+
 
 }
 makenomove(){
@@ -42,13 +52,15 @@ this.colors.forEach(color => {
 }
 }
 
-class Piece extends GameState{
+class Piece{
 constructor(color, position){
     this.position=position;
     this.color=color;
     this.potentialmove=0;
+    this.startingpositions=[1,8,15,22];
 }
 potentialmovement(roll){
+    var diff=0;
     //28 spaces on the board not counting home spaces or finish areas
     //positions in finishing spaces labelled in the 100s
     //check if position is a home space (in the 40s up to 75)
@@ -70,15 +82,51 @@ potentialmovement(roll){
 
         }
     }
+    //finishing space checks
+
+    //check if red can move into finishing spaces 
+    //red finishing spaces are 101-104
+    else if((this.color=="red")&&((this.position + roll)>28&&(this.position+roll)<=32)){
+        diff=((this.position+roll)-28);
+        this.potentialmove=100+diff;
+    }
+
+    //check if green can move into finishing spaces
+    //green finishing spaces are 121-124
+    else if((this.color=="green")&&((this.position + roll)>7&&(this.position+roll)<=11)){
+        diff=((this.position+roll)-7);
+        this.potentialmove=120+diff;
+    }
+
+     //check if blue can move into finishing spaces
+     //blue finishing spaces are 131-134
+     else if((this.color=="blue")&&((this.position + roll)>14&&(this.position+roll)<=18)){
+        diff=((this.position+roll)-14);
+        this.potentialmove=130+diff;
+    }
+
+     //check if yellow can move into finishing spaces
+     //yellow finishing spaces are 111-114
+     else if((this.color=="yellow")&&((this.position + roll)>21&&(this.position+roll)<=25)){
+        diff=((this.position+roll)-21);
+        this.potentialmove=110+diff;
+    }
+
+    //normal potential movements
+    else{
+        this.potentialmovement=this.position+roll;
+    }
+    
 
 
 }
 }
 
-class Controller extends GameState{
+class Controller{
     constructor(){
         //create new game
-        gs=new GameState();
+        this.gs=new GameState();
+        this.gs.piececreation();
 
     }
 }
